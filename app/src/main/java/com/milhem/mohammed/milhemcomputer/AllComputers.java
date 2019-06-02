0d6cde4b504144f17c1b8259326ebb6f8c96a07d
moaad <rakaz.hammad12@gmail.com>
2019-06-02 12:16:16 +0300

    aa

--- app/src/main/java/com/milhem/mohammed/milhemcomputer/AllComputers.java
+++ app/src/main/java/com/milhem/mohammed/milhemcomputer/AllComputers.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -16,7 +17,14 @@ public class AllComputers extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_computers);
         btnAdd = (Button) findViewById(R.id.btnAdd);
+       initListview();
+    }
+    private void  initListview()
+    {
         ListV = (ListView) findViewById(R.id.ListV);
+        String [] a={"one","two","three"};
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,a);
+        ListV.setAdapter(adapter);
     }
     public void onClick(View v)
     {
